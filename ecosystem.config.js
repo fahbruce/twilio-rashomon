f798b904f31defb39cbe860bd5f94c2e7f9e0f71
Fahbruce <fahbruceroland@gmail.com>
2022-04-08 12:55:09 +0300

    Deploy app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,5 +4,18 @@ module.exports = {
       script : "./server.js",
       args   : "limit",
       watch  : '.'
-    }]
-  }
+    }],
+
+    deploy : {
+        production : {
+            user : 'user ID',
+            host : 'host ou ip du server',
+            ref  : 'origin/main',
+            repo : 'repository git',
+            path : 'path server où il ya le projet',
+            'pre-deploy-local': '',
+            'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --eny production',
+            'pre-setup': ''
+        }
+    }
+  };
